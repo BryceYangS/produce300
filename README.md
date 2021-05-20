888a0ca513cdb72ef915cdc62209f7a44b332048
Bryce Yang <nash31@korea.ac.kr>
2021-05-20 20:22:57 +0900

    refactor:HTTP 캐시 헤더 적용
    HTTP 캐시 적용을 통해 네트워크 통신 줄임

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Produce300 Node.js -> Spring Boot 전환
- 
 
 ## 1.환경구성
+
 - Spring Boot 2.3.1.RELEASE
 - Java : 14
 - Gradle
@@ -10,16 +10,29 @@
 - Docker 배포
 
 ## 2. Docker
+
 - Spring Boot
     * jar 파일 생성 후 Dockerfile을 통해 이미지 빌드 및 배포
     * 다른 컨테이너에 있는 몽고DB와 통신하기 위해 몽고DB와 동일한 network에 연결
-    > docker network create [네트워크명]    
-    
-    > docker network connect [네트워크명] [컨테이너명]
+  > docker network create [네트워크명]
+
+  > docker network connect [네트워크명] [컨테이너명]
 
     * 참조 : [https://docs.docker.com/network/bridge/](https://docs.docker.com/network/bridge/)
     * mongo DB의 host 값 변경 : localhost -> mongo
-    
+
 - Mongo DB
     * `kge` DB를 생성하고 특정 아이디에 읽기 권한 부여. 스프링부트 프로젝트에서 read만 하고 있기 때문에 읽기 권한 있는 아이디로 접속 하도록 설정
     * DB 데이터는 로컬에 저장되도록 로컬 디렉토리를 마운트
+
+## HTTP 캐시 적용
+
+- Redis 캐시서버 대신, HTTP 캐시를 적용
+
+```java
+  CacheControl cacheControl=CacheControl.maxAge(99999,TimeUnit.SECONDS);
+
+	return ResponseEntity.status(HttpStatus.OK)
+	.cacheControl(cacheControl)
+	.body(result);
+```
--- src/main/java/com/kge/produce300/controller/Produce300Controller.java
+++ src/main/java/com/kge/produce300/controller/Produce300Controller.java
@@ -1,5 +1,23 @@
 package com.kge.produce300.controller;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.google.gson.JsonObject;
 import com.kge.produce300.domain.dto.CandidateDTO;
 import com.kge.produce300.service.Produce300Service;
@@ -8,77 +26,72 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.apache.commons.io.IOUtils;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
-import java.io.IOException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class Produce300Controller {
 
-    private final Produce300Service produce300Service;
+	private final Produce300Service produce300Service;
 
-    @ApiOperation(value = "기본 데이터", httpMethod = "GET", notes = "행정동,당선인 데이터")
-    @GetMapping("/api/data")
-    public ResponseEntity<Object> getData() throws Exception {
-        log.debug("기본 데이터 Controller 시작");
-        CacheControl cacheControl = CacheControl.maxAge(9999, TimeUnit.SECONDS);
-        return ResponseEntity.ok()
-            .cacheControl(cacheControl)
-            .body(produce300Service.retrieveBaseData());
-    }
+	@ApiOperation(value = "기본 데이터", httpMethod = "GET", notes = "행정동,당선인 데이터")
+	@GetMapping("/api/data")
+	public ResponseEntity<Object> getData() throws Exception {
+		CacheControl cacheControl = CacheControl.maxAge(9999, TimeUnit.SECONDS);
+		return ResponseEntity.ok()
+			.cacheControl(cacheControl)
+			.body(produce300Service.retrieveBaseData());
+	}
 
+	@ApiOperation(value = "21대 후보자 정보", httpMethod = "GET", notes = "21대 후보자 정보")
+	@GetMapping("/api/candidate")
+	public ResponseEntity<Object> getCandidateData(@RequestParam String sggCode) throws Exception {
 
-    @ApiOperation(value = "21대 후보자 정보", httpMethod = "GET", notes = "21대 후보자 정보")
-    @GetMapping("/api/candidate")
-    public ResponseEntity<Object> getCandidateData(@RequestParam String sggCode) throws Exception {
-        Map<String, Object> result = new HashMap<>();
-        List<CandidateDTO> candidates = produce300Service.retrieveCandidates(sggCode);
-        result.put("candidates", candidates);
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
+		Map<String, Object> result = new HashMap<>();
+		List<CandidateDTO> candidates = produce300Service.retrieveCandidates(sggCode);
+		result.put("candidates", candidates);
 
-    @ApiOperation(value = "후보자 이미지", httpMethod = "GET", notes = "선관위 이미지 stream 변환")
-    @GetMapping( value = "/api/candidateImg", produces = MediaType.IMAGE_JPEG_VALUE)
-    public @ResponseBody byte[] convertCandidateImg(@RequestParam String url) throws IOException {
-        JsonObject result = new JsonObject();
-        URL urlObject = new URL(url);
-        return IOUtils.toByteArray(urlObject.openStream());
-    }
+		CacheControl cacheControl = CacheControl.maxAge(99999, TimeUnit.SECONDS);
+		return ResponseEntity.status(HttpStatus.OK)
+			.cacheControl(cacheControl)
+			.body(result);
+	}
 
+	@ApiOperation(value = "후보자 이미지", httpMethod = "GET", notes = "선관위 이미지 stream 변환")
+	@GetMapping(value = "/api/candidateImg", produces = MediaType.IMAGE_JPEG_VALUE)
+	@ResponseBody
+	public byte[] convertCandidateImg(@RequestParam String url) throws IOException {
+		URL urlObject = new URL(url);
+		return IOUtils.toByteArray(urlObject.openStream());
+	}
 
-    // TO-DO
-    @ApiOperation(value = "후보자 전체 데이터", httpMethod = "GET", notes = "후보자 전체 데이터")
-    @GetMapping("/api/allCandidate")
-    public ResponseEntity<Object> getAllCandidateData() throws Exception{
-        return new ResponseEntity<>(produce300Service.getAllCandidateData(), HttpStatus.OK);
-    }
+	@ApiOperation(value = "후보자 전체 데이터", httpMethod = "GET", notes = "후보자 전체 데이터")
+	@GetMapping("/api/allCandidate")
+	public ResponseEntity<Object> getAllCandidateData() throws Exception {
 
-    // TO-DO
-    @ApiOperation(value = "투표소 목록", httpMethod = "GET", notes = "투표소 목록")
-    @GetMapping("/api/stns")
-    public ResponseEntity<Object> getStns(@RequestBody Map<String, Object> param) throws Exception {
-        JsonObject result = new JsonObject();
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
+		Map<String, Object> result = new HashMap<>();
+		List<CandidateDTO> data = produce300Service.getAllCandidateData();
+		result.put("data", data);
 
-    // TO-DO
-    @ApiOperation(value = "투표율", httpMethod = "GET", notes = "투표율")
-    @GetMapping("/api/voteRate")
-    public ResponseEntity<Object> getVoteRate() {
-        JsonObject result = new JsonObject();
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
+		CacheControl cacheControl = CacheControl.maxAge(99999, TimeUnit.SECONDS);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.cacheControl(cacheControl)
+			.body(result);
+	}
+
+	// TODO
+	@ApiOperation(value = "투표소 목록", httpMethod = "GET", notes = "투표소 목록")
+	@GetMapping("/api/stns")
+	public ResponseEntity<Object> getStns(@RequestBody Map<String, Object> param) throws Exception {
+		JsonObject result = new JsonObject();
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	// TODO
+	@ApiOperation(value = "투표율", httpMethod = "GET", notes = "투표율")
+	@GetMapping("/api/voteRate")
+	public ResponseEntity<Object> getVoteRate() {
+		JsonObject result = new JsonObject();
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
 }
